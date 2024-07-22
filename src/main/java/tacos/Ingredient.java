5299f9b13c052a94f6886ddf9383250bdc001372
rudoy <rudoy@nca.by>
2024-07-22 17:22:07 +0300

    feat: add security and db

--- src/main/java/tacos/Ingredient.java
+++ src/main/java/tacos/Ingredient.java
@@ -8,7 +8,6 @@ import lombok.*;
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PRIVATE, force = true)
-@RequiredArgsConstructor
 public class Ingredient {
 
     @Id
--- src/main/java/tacos/data/OrderRepository.java
+++ src/main/java/tacos/data/OrderRepository.java
@@ -1,22 +1,9 @@
 package tacos.data;
 
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import tacos.TacoOrder;
 
-import java.util.Date;
-import java.util.List;
 
 public interface OrderRepository extends CrudRepository<TacoOrder, Long> {
 
-    List<TacoOrder> findByDeliveryZip(String deliveryZip);
-
-    List<TacoOrder> readTacoOrdersByDeliveryZipAndPlacedAtBetween(String deliveryZip, Date startDate, Date endDate);
-
-    List<TacoOrder> findByDeliveryToAndDeliveryCityAllIgnoresCase(String deliveryTo, String deliveryCity);
-
-    List<TacoOrder> findByDeliveryCityOrderByDeliveryTo(String city);
-
-    @Query("Order o where o.deliveryCity='Seattle'")
-    List<TacoOrder> readOrdersDeliveredInSeattle();
 }
